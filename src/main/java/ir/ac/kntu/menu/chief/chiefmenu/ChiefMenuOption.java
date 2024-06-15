fd7653e7397244bcee762088474cfe61852a7d43
Matltin <ma1383ahmadi@gmail.com>
2024-06-15 18:48:57 +0330

    feat : add chief menu option

--- src/main/java/ir/ac/kntu/menu/chief/chiefmenu/ChiefMenuOption.java
+++ src/main/java/ir/ac/kntu/menu/chief/chiefmenu/ChiefMenuOption.java
@@ -1,4 +1,15 @@
 package ir.ac.kntu.menu.chief.chiefmenu;
 
-public class ChiefMenuOption {
+public enum ChiefMenuOption {
+    MANAGE_USERS,
+    AUTO_TRANSACTION,
+    SETTING,
+    BACK;
+
+    public static void printOption() {
+        System.out.println("\n1.Manage users\n" +
+                "2.Auto transaction\n" +
+                "3.Setting\n" +
+                "4.Back\n");
+    }
 }
