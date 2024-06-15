fc6919217760365b1adfab58060ab9ba9ad90b7b
Matltin <ma1383ahmadi@gmail.com>
2024-06-15 17:43:11 +0330

    feat : add SimCardDB

---
+++ src/main/java/ir/ac/kntu/db/SimCardDB.java
@@ -0,0 +1,38 @@
+package ir.ac.kntu.db;
+
+import ir.ac.kntu.phone.Phone;
+
+import java.util.Set;
+
+public class SimCardDB {
+    private Set<Phone> phones;
+
+    public SimCardDB(Set<Phone> phones) {
+        this.phones = phones;
+    }
+
+    public Set<Phone> getPhones() {
+        return phones;
+    }
+
+    public void addPhone(Phone phone) {
+        phones.add(phone);
+    }
+
+    public void remove(Phone phone) {
+        phones.remove(phone);
+    }
+
+    public boolean contain(Phone phone) {
+        return phones.contains(phone);
+    }
+
+    public Phone findPhone(String phoneNumber) {
+        for(Phone phone : phones) {
+            if(phone.getPhoneNumber().equals(phoneNumber)) {
+                return phone;
+            }
+        }
+        return null;
+    }
+}
