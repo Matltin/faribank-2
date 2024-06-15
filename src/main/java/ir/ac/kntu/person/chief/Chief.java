25b12a4a9bf1449aa678c8ad7256a86a1ad331ad
Matltin <ma1383ahmadi@gmail.com>
2024-06-15 18:11:11 +0330

    feat : complect chief class

--- src/main/java/ir/ac/kntu/person/chief/Chief.java
+++ src/main/java/ir/ac/kntu/person/chief/Chief.java
@@ -1,4 +1,48 @@
 package ir.ac.kntu.person.chief;
 
-public class Chief {
+import ir.ac.kntu.person.Person;
+
+public class Chief extends Person {
+    private String userName;
+    private int position;
+    private State state;
+
+    public Chief(String firstName, String lastName, String password, String userName, int position) {
+        super(firstName, lastName, password);
+        this.userName = userName;
+        this.position = position;
+        state = State.UNBLOCKED;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Chief{" +
+                super.toString() +
+                "userName='" + userName + '\'' +
+                '}';
+    }
 }
--- src/main/java/ir/ac/kntu/person/chief/State.java
+++ src/main/java/ir/ac/kntu/person/chief/State.java
@@ -1,4 +1,5 @@
 package ir.ac.kntu.person.chief;
 
 public enum State {
+    
 }
