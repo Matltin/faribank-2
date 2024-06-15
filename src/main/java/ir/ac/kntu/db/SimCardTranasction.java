68117aa45cca2378b8e13ce0020d20cf6f9a6977
Matltin <ma1383ahmadi@gmail.com>
2024-06-15 17:44:01 +0330

    feat : add SimCardTransactionDB

---
+++ src/main/java/ir/ac/kntu/db/SimCardTranasction.java
@@ -0,0 +1,36 @@
+package ir.ac.kntu.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimCardTransactionDB {
+    private List<SimCard> simCards;
+
+    public SimCardTransactionDB() {
+        this.simCards = new ArrayList<>();
+    }
+
+    public void addSim(SimCard simCard) {
+        simCards.add(simCard);
+    }
+
+    public void removeSim(SimCard simCard) {
+        simCards.remove(simCard);
+    }
+
+    public List<SimCard> getSimCards() {
+        return simCards;
+    }
+
+    public void setSimCards(List<SimCard> simCards) {
+        this.simCards = simCards;
+    }
+
+    public void printTransaction() {
+        int counter = 1;
+        for(SimCard simCard : simCards) {
+            System.out.println(counter + "." + simCard);
+            counter++;
+        }
+    }
+}
