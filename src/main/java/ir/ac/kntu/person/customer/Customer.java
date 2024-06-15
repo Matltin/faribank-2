6e6773ab0e84b72c371faa859bd06bdd20af389e
Matltin <ma1383ahmadi@gmail.com>
2024-06-15 18:05:44 +0330

    chore : change customer class for paya pole and card

--- src/main/java/ir/ac/kntu/person/customer/Customer.java
+++ src/main/java/ir/ac/kntu/person/customer/Customer.java
@@ -5,31 +5,37 @@ import ir.ac.kntu.db.*;
 import ir.ac.kntu.faribank.account.Account;
 import ir.ac.kntu.person.ContactPerson;
 import ir.ac.kntu.person.Person;
+import ir.ac.kntu.phone.Phone;
 
 import java.util.Objects;
 import java.util.Random;
 
 public class Customer extends Person {
-
+    
     private String iDocument;
-    private String phoneNumber;
+    private Phone phone;
     private Account account;
     private ContactPersonDB contactPersonDB;
     private RecentTransactionDB recentTransaction;
     private boolean contactAvailable;
     private State state;
     private MessageDB messageDB;
+    private SimCardTransactionDB simCardTransactionDB;
 
-    public Customer(String firstName, String lastName, String password, String iDocument, String phoneNumber) {
+    public Customer(String firstName, String lastName, String password, String iDocument, String phoneNumber, SimCardDB simCardDB) {
         super(firstName, lastName, password);
         this.iDocument = iDocument;
-        this.phoneNumber = phoneNumber;
+        this.phone = new Phone(phoneNumber, simCardDB);
+        if(!simCardDB.contain(phone)) {
+            simCardDB.addPhone(phone);
+        }
         contactPersonDB = new ContactPersonDB();
         recentTransaction = new RecentTransactionDB();
         contactAvailable = true;
         state = State.IN_PROGRESSING;
         messageDB = new MessageDB();
         account = new Account(0, randomAccountNO());
+        simCardTransactionDB = new SimCardTransactionDB();
     }
 
     public String getIDocument() {
@@ -41,11 +47,11 @@ public class Customer extends Person {
     }
 
     public String getPhoneNumber() {
-        return phoneNumber;
+        return phone.getPhoneNumber();
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        this.phone.setPhoneNumber(phoneNumber);
     }
 
     public Account getAccount() {
@@ -96,6 +102,22 @@ public class Customer extends Person {
         this.messageDB = messageDB;
     }
 
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+
+    public SimCardTransactionDB getSimCardTransactionDB() {
+        return simCardTransactionDB;
+    }
+
+    public void setSimCardTransactionDB(SimCardTransactionDB simCardTransactionDB) {
+        this.simCardTransactionDB = simCardTransactionDB;
+    }
+
     public void addContactPerson(String firstName, String lastName, String phoneNumber, CustomerDB customerDB) {
         String accountNumber = "";
         for (Customer customer : customerDB.getCustomers()) {
@@ -104,7 +126,7 @@ public class Customer extends Person {
                 break;
             }
         }
-        Customer customer = customerDB.findCustomer(accountNumber);
+        Customer customer = customerDB.findCustomerByAccountNO(accountNumber);
         if (customer.state != State.ACCEPTED) {
             System.out.println(Constance.RED + "There is no customer!!" + Constance.RESET);
             return;
@@ -130,7 +152,7 @@ public class Customer extends Person {
     public String toString() {
         return "Customer{" +
                 super.toString() +
-                "phoneNumber='" + phoneNumber + '\'' +
+                "phoneNumber='" + phone.getPhoneNumber() + '\'' +
                 ", iDocument='" + iDocument + '\'' +
                 ", state=" + state +
                 '}';
@@ -138,18 +160,14 @@ public class Customer extends Person {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
         Customer customer = (Customer) obj;
-        return contactAvailable == customer.contactAvailable && Objects.equals(iDocument, customer.iDocument) && Objects.equals(phoneNumber, customer.phoneNumber) && Objects.equals(account, customer.account) && Objects.equals(contactPersonDB, customer.contactPersonDB);
+        return Objects.equals(iDocument, customer.iDocument) && Objects.equals(phone, customer.phone) && Objects.equals(account, customer.account) && Objects.equals(contactPersonDB, customer.contactPersonDB) && Objects.equals(recentTransaction, customer.recentTransaction) && Objects.equals(messageDB, customer.messageDB);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(iDocument, phoneNumber, account, contactPersonDB, contactAvailable);
+        return Objects.hash(iDocument, phone, account, contactPersonDB, recentTransaction, messageDB);
     }
 }
