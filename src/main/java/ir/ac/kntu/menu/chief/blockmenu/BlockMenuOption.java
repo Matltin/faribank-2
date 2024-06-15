a0b0290ece227f7cac1136c074ef876846d7a615
Matltin <ma1383ahmadi@gmail.com>
2024-06-15 18:47:22 +0330

    feat : add block user option

--- src/main/java/ir/ac/kntu/menu/chief/blockmenu/BlockMenuOption.java
+++ src/main/java/ir/ac/kntu/menu/chief/blockmenu/BlockMenuOption.java
@@ -1,2 +1,13 @@
-package ir.ac.kntu.menu.chief.blockmenu;public enum BlockMenuOption {
+package ir.ac.kntu.menu.chief.blockmenu;
+
+public enum BlockMenuOption {
+    ADMIN,
+    CHIEF,
+    BACK;
+
+    public static void printOption() {
+        System.out.println("\n1.Admin\n" +
+                "2.Chief\n" +
+                "3.Back\n");
+    }
 }
