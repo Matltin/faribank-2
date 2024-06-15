9b7aaa45ebab6dc1b72c2d76f9c31c35955c3eb9
Matltin <ma1383ahmadi@gmail.com>
2024-06-15 17:38:24 +0330

    chore : change transaction db

--- src/main/java/ir/ac/kntu/db/TransactionDB.java
+++ src/main/java/ir/ac/kntu/db/TransactionDB.java
@@ -4,6 +4,7 @@ import ir.ac.kntu.transaction.Transaction;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class TransactionDB {
     private List<Transaction> transactions;
@@ -41,5 +42,18 @@ public class TransactionDB {
         return "TransactionDB{" +
                 "transactions=" + transactions +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        TransactionDB that = (TransactionDB) obj;
+        return Objects.equals(transactions, that.transactions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(transactions);
     }
 }
