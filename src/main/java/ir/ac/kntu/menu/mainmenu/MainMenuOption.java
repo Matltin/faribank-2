36ec83729e6682c9809f28007391a239c00b1be3
Matltin <ma1383ahmadi@gmail.com>
2024-06-15 18:34:04 +0330

    chore : change MainMenuOption

--- src/main/java/ir/ac/kntu/menu/mainmenu/MainMenuOption.java
+++ src/main/java/ir/ac/kntu/menu/mainmenu/MainMenuOption.java
@@ -3,11 +3,13 @@ package ir.ac.kntu.menu.mainmenu;
 public enum MainMenuOption {
     ADMIN,
     CUSTOMER,
+    CHIEF,
     EXIT;
 
     public static void printOption() {
         System.out.println("\n1.Admin\n" +
                 "2.Customer\n" +
-                "3.Exit\n");
+                "3.Chief\n" +
+                "4.Exit\n");
     }
 }
